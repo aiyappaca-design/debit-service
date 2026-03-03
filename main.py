f6fb529ad4f762f8fa5731c513247e70320a2262
Aiyappa C A <aiyappaca@gmail.com>
2026-03-03 13:54:44 +0530

    Ninth commit

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ def root():
 @app.get("/debit/{card_id}/status")
 async def get_status(card_id: str, user=Depends(verify_token)):
     async with httpx.AsyncClient() as client:
+        try:
+            headers = {
+                "Authorization": request.headers.get("Authorization")
+            }
         response = await client.get(url=f"{CARD_SERVICE_URL}/cards/{card_id}",
                 headers=headers,   # ✅ Propagate Token
             )
