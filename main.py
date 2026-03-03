9103380917cc011605e784cc626fba0948dd5cfd
Aiyappa C A <aiyappaca@gmail.com>
2026-03-03 14:13:39 +0530

    Ninth commit

--- main.py
+++ main.py
@@ -29,14 +29,14 @@ async def get_status(card_id: str, user=Depends(verify_token)):
             headers = {
                 "Authorization": request.headers.get("Authorization")
             }
-        response = await client.get(url=f"{CARD_SERVICE_URL}/cards/{card_id}",
+        response = await client.get(f"{CARD_SERVICE_URL}/cards/{card_id}",
                 headers=headers,   # ✅ Propagate Token
             )
 
-        if response.status_code != 200:
-            raise HTTPException(status_code=response.status_code, detail="Card service error")
-
         return {"status": response.json()["status"]}
+
+        except httpx.RequestError:
+            raise HTTPException(status_code=response.status_code, detail="Card service error")
 
 
 @app.patch("/debit/{card_id}/status")
