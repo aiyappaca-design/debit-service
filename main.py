3bf34414ab2af1bbe277c33b07932ba308ef0fd9
Aiyappa C A <aiyappaca@gmail.com>
2026-03-03 15:02:40 +0530

    Ninth commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Request
 from pydantic import BaseModel
 import httpx
 import os
@@ -22,16 +22,22 @@ def root():
     return {"message": "Debit Service Running"}
 
 
+# -------------------------------
+# GET STATUS
+# -------------------------------
 @app.get("/debit/{card_id}/status")
-async def get_status(card_id: str, user=Depends(verify_token)):
-
+async def get_status(
+    card_id: str,
+    request: Request,
+    user=Depends(verify_token)
+):
     auth_header = request.headers.get("Authorization")
 
     async with httpx.AsyncClient() as client:
-
         response = await client.get(
             f"{CARD_SERVICE_URL}/cards/{card_id}",
-            headers={"Authorization": auth_header})
+            headers={"Authorization": auth_header}
+        )
 
         if response.status_code != 200:
             raise HTTPException(status_code=response.status_code, detail="Card service error")
@@ -39,12 +45,23 @@ async def get_status(card_id: str, user=Depends(verify_token)):
         return {"status": response.json()["status"]}
 
 
+# -------------------------------
+# UPDATE STATUS
+# -------------------------------
 @app.patch("/debit/{card_id}/status")
-async def update_status(card_id: str, body: StatusUpdate, user=Depends(verify_token)):
+async def update_status(
+    card_id: str,
+    body: StatusUpdate,
+    request: Request,
+    user=Depends(verify_token)
+):
+    auth_header = request.headers.get("Authorization")
+
     async with httpx.AsyncClient() as client:
         response = await client.put(
             f"{CARD_SERVICE_URL}/cards/{card_id}",
-            json={"status": body.status}
+            json={"status": body.status},
+            headers={"Authorization": auth_header}
         )
 
         if response.status_code != 200:
@@ -53,12 +70,23 @@ async def update_status(card_id: str, body: StatusUpdate, user=Depends(verify_to
         return {"message": "Status updated"}
 
 
+# -------------------------------
+# UPDATE LIMITS
+# -------------------------------
 @app.patch("/debit/{card_id}/limits")
-async def update_limits(card_id: str, body: LimitUpdate, user=Depends(verify_token)):
+async def update_limits(
+    card_id: str,
+    body: LimitUpdate,
+    request: Request,
+    user=Depends(verify_token)
+):
+    auth_header = request.headers.get("Authorization")
+
     async with httpx.AsyncClient() as client:
         response = await client.put(
             f"{CARD_SERVICE_URL}/cards/{card_id}",
-            json={"transaction_limit": body.transaction_limit}
+            json={"transaction_limit": body.transaction_limit},
+            headers={"Authorization": auth_header}
         )
 
         if response.status_code != 200:
